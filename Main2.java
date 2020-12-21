f3fb46f09e65c46439df92a9f0ec400b8388d342
Hikari Yoneyama <e205743@ie.u-ryukyu.ac.jp>
2020-12-21 11:26:11 +0900

    Step3

--- Main2.java
+++ Main2.java
@@ -3,7 +3,7 @@ public class Main2 {
     public static void main(String[] args){
         
         String str = "百二十三";
-        int value = Integer.parselnt(str);
+        int value = Integer.parseInt(str);
 
     }
 
